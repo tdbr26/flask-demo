533de563434f9e6db9d8638e28949925bac7cda4
Thabit Reid <thabitreid@thabits-Mac-mini.local>
2015-11-10 15:16:45 -0400

    configured sign in and out

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
     first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
@@ -8,3 +8,8 @@ class SignupForm(Form):
     email = StringField('Email', validators=[DataRequired("Please enter a valid email address"), Email("Enter email in the format user@example.com")])
     password = PasswordField('Password', validators=[DataRequired("Please create a password")])
     submit = SubmitField('Sign up')
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired("Enter email address"), Email("Enter email in the format user@example.com")])
+    password = PasswordField('Password', validators=[DataRequired("Enter a password")])
+    submit = SubmitField("Sign in")
